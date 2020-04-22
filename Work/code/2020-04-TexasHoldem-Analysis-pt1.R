c09386b3721b32e42f8a35586a236bff9cfe2aab
christopher <cmaerzlu@rand.org>
2020-04-22 10:52:47 -0700

    Final Draft

--- Work/code/2020-04-TexasHoldem-Analysis-pt1.R
+++ Work/code/2020-04-TexasHoldem-Analysis-pt1.R
@@ -206,135 +206,163 @@ win_distribution_2players <- hand_win_plots[[1]]
 win_distribution_10players <- hand_win_plots[[9]]
 
 #########################################################################################################################
-# Plots 3 - 5 ###########################################################################################################
+# Plots 3 ###############################################################################################################
 # Data Calculation
-# beaters <- function(gList, hand) {
-#   table_sizes <- as.numeric(gsub("players", "", names(gList)))
-#   tab <- do.call(rbind, lapply(seq_along(gList), function(x, df_list, n_players) {
-#     df <- df_list[[x]]
-#     loses <- df[df$hand_id %in% df$hand_id[df$winner == FALSE & df$final_hand_type == hand], ]
-#     dta <- as.data.frame(prop.table(table(loses$final_hand_type, loses$winner), margin = 2))
-#     colnames(dta) <- c("Hand", "Win", "Percentage")
-#     dta <- dta[dta$Win == TRUE & dta$Percentage > 0, ]
-#     dta$Players <- factor(n_players[x], levels = 2:10)
-#     return(dta)
-#   }, df_list = gList, n_players = table_sizes))
-#   return(tab)
-# }
-# # Plots
-# # fullhouse_loses <- beaters(games_list, "Full House")
-# # fullhouse_loses$Hand <- droplevels(fullhouse_loses$Hand)
-# # fullhouse_loses <- complete(fullhouse_loses, Hand, Players)
-# # lose_fullhouse <- ggplot(data = fullhouse_loses) +
-# #   geom_bar(aes(x = Players, y = Percentage, fill = Hand), stat = "identity", position = "dodge") +
-# #   geom_hline(yintercept = c(1, .75, .5, .25), linetype = "dashed", color = plt_wordscolor) +
-# #   scale_fill_manual("Winning Hand", values = plt_colordiscr6) +
-# #   scale_y_continuous(labels = percent, limits = c(0, 1)) +
-# #   ggtitle("When a Full House Loses") + plt_theme
-# flush_loses <- beaters(games_list, "Flush")
-# flush_loses$Hand <- droplevels(flush_loses$Hand)
-# flush_loses <- complete(flush_loses, Hand, Players)
-# lose_flush <- ggplot(data = flush_loses) +
-#   geom_bar(aes(x = Players, y = Percentage, fill = Hand), stat = "identity", position = "dodge") +
-#   geom_hline(yintercept = c(1, .75, .5, .25), linetype = "dashed", color = plt_wordscolor) +
-#   scale_fill_manual("Winning Hand", values = plt_colordiscr6) +
-#   scale_y_continuous(labels = percent, limits = c(0, 1)) +
-#   ggtitle("When a Flush Loses") + plt_theme
-# straight_loses <- beaters(games_list, "Straight")
-# straight_loses$Hand <- droplevels(straight_loses$Hand)
-# straight_loses <- complete(straight_loses, Hand, Players)
-# lose_straight <- ggplot(data = straight_loses) +
-#   geom_bar(aes(x = Players, y = Percentage, fill = Hand), stat = "identity", position = "dodge") +
-#   geom_hline(yintercept = c(1, .75, .5, .25), linetype = "dashed", color = plt_wordscolor) +
-#   scale_fill_manual("Winning Hand", values = plt_colordiscr6) +
-#   scale_y_continuous(labels = percent, limits = c(0, 1)) +
-#   ggtitle("When a Straight Loses") + plt_theme
-# kind3_loses <- beaters(games_list, "Three of a Kind")
-# kind3_loses$Hand <- droplevels(kind3_loses$Hand)
-# kind3_loses <- complete(kind3_loses, Hand, Players)
-# lose_3kind <- ggplot(data = kind3_loses) +
-#   geom_bar(aes(x = Players, y = Percentage, fill = Hand), stat = "identity", position = "dodge") +
-#   geom_hline(yintercept = c(1, .75, .5, .25), linetype = "dashed", color = plt_wordscolor) +
-#   scale_fill_manual("Winning Hand", values = plt_colordiscr6) +
-#   scale_y_continuous(labels = percent, limits = c(0, 1)) +
-#   ggtitle("When a Three of a Kind Loses") + plt_theme
+beaters <- function(gList, hand) {
+  table_sizes <- as.numeric(gsub("players", "", names(gList)))
+  tab <- do.call(rbind, lapply(seq_along(gList), function(x, df_list, n_players) {
+    df <- df_list[[x]]
+    loses <- df[df$hand_id %in% df$hand_id[df$winner == FALSE & df$final_hand_type == hand], ]
+    dta <- as.data.frame(prop.table(table(loses$final_hand_type, loses$winner), margin = 2))
+    colnames(dta) <- c("Hand", "Win", "Percentage")
+    dta <- dta[dta$Win == TRUE & dta$Percentage > 0, ]
+    dta$Players <- factor(n_players[x], levels = 2:10)
+    return(dta)
+  }, df_list = gList, n_players = table_sizes))
+  return(tab)
+}
+fullhouse_loses <- beaters(games_list, "Full House")
+flush_loses <- beaters(games_list, "Flush")
+straight_loses <- beaters(games_list, "Straight")
+kind3_loses <- beaters(games_list, "Three of a Kind")
+fullhouse_loses$Loser <- "Full House"
+flush_loses$Loser <- "Flush"
+straight_loses$Loser <- "Straight"
+kind3_loses$Loser <- "Three of a\nKind"
+losers_8players <- rbind(fullhouse_loses, flush_loses, straight_loses, kind3_loses)
+losers_8players <- losers_8players[losers_8players$Players == 8, ]
+losers_8players$Hand <- droplevels(losers_8players$Hand)
+losers_8players <- complete(losers_8players, Hand, Loser)
+losers_8players$Percentage[is.na(losers_8players$Percentage)] <- 0
+losers_8players$Loser <- factor(losers_8players$Loser, levels = gsub("Three of a Kind", "Three of a\nKind", hand_lvls))
+losers_8players$Loser <- droplevels(losers_8players$Loser)
+
+# Plot
+beaters_8players <- ggplot(data = losers_8players) +
+  geom_bar(aes(x = Loser, y = Percentage, fill = Hand), stat = "identity", position = "dodge") +
+  geom_hline(yintercept = c(1, .75, .5, .25), linetype = "dashed", color = plt_wordscolor) +
+  scale_fill_manual("Winning Hand", values = plt_colordiscr6) +
+  scale_y_continuous(labels = percent, limits = c(0, 1)) +
+  labs(title = "What Hands to Watch Out For") + plt_theme
 
 #########################################################################################################################
-# Plots 6 - 8 ###########################################################################################################
+# Plot 4 - 5 ################################################################################################################
 # Data Calculation
-# pocket_information <- do.call(rbind, lapply(games_list, function(x) {
-#   # title <- "Pocket Cards Chances of Winning"
-#   x$pocket_card1 <- x$pocket_card1 %% 100
-#   x$pocket_card2 <- x$pocket_card2 %% 100
-#   x$Win <- ifelse(x$winner == TRUE, "Winner", "Loser")
-#   tmp <- x %>% group_by(pocket_card1, pocket_card2, suited_pocket, Win) %>%
-#     summarise(N = n()) %>%
-#     spread(Win, N, fill = 0) %>%
-#     arrange(pocket_card1, pocket_card2)
-#   tmp <- left_join(tmp, tmp, by = c("pocket_card1" = "pocket_card2", "pocket_card2" = "pocket_card1", "suited_pocket"))
-#   tmp$Winner <- ifelse(tmp$pocket_card1 == tmp$pocket_card2, tmp$Winner.x, tmp$Winner.x + tmp$Winner.y)
-#   tmp$Loser <- ifelse(tmp$pocket_card1 == tmp$pocket_card2, tmp$Loser.x, tmp$Loser.x + tmp$Loser.y)
-#   tmp <- tmp[, c("pocket_card1", "pocket_card2", "suited_pocket", "Winner", "Loser")]
-#   tmp$pct_get <- (tmp$Winner + tmp$Loser)/sum(tmp[, c("Winner", "Loser")])
-#   tmp$pct_win <- tmp$Winner/rowSums(tmp[, c("Winner", "Loser")])
-#   # Pairs
-#   tmp1 <- tmp[tmp$pocket_card1 == tmp$pocket_card2, ]
-#   # Unique Suited Cards
-#   tmp2 <- tmp[tmp$suited_pocket & tmp$pocket_card1 != tmp$pocket_card2, ]
-#   tmp2 <- tmp2[tmp2$pocket_card1 <= tmp2$pocket_card2, ]
-#   # Unique Non-suited Cards
-#   tmp3 <- tmp[!tmp$suited_pocket & tmp$pocket_card1 != tmp$pocket_card2, ]
-#   tmp3 <- tmp3[tmp3$pocket_card1 > tmp3$pocket_card2, ]
-#   tmp.new <- rbind(tmp1, tmp2, tmp3)
+pocket_information <- do.call(rbind, lapply(games_list, function(x) {
+  # title <- "Pocket Cards Chances of Winning"
+  x$pocket_card1 <- x$pocket_card1 %% 100
+  x$pocket_card2 <- x$pocket_card2 %% 100
+  x$Win <- ifelse(x$winner == TRUE, "Winner", "Loser")
+  tmp <- x %>% group_by(pocket_card1, pocket_card2, suited_pocket, Win) %>%
+    summarise(N = n()) %>%
+    spread(Win, N, fill = 0) %>%
+    arrange(pocket_card1, pocket_card2)
+  tmp <- left_join(tmp, tmp, by = c("pocket_card1" = "pocket_card2", "pocket_card2" = "pocket_card1", "suited_pocket"))
+  tmp$Winner <- ifelse(tmp$pocket_card1 == tmp$pocket_card2, tmp$Winner.x, tmp$Winner.x + tmp$Winner.y)
+  tmp$Loser <- ifelse(tmp$pocket_card1 == tmp$pocket_card2, tmp$Loser.x, tmp$Loser.x + tmp$Loser.y)
+  tmp <- tmp[, c("pocket_card1", "pocket_card2", "suited_pocket", "Winner", "Loser")]
+  tmp$pct_get <- (tmp$Winner + tmp$Loser)/sum(tmp[, c("Winner", "Loser")])
+  tmp$pct_win <- tmp$Winner/rowSums(tmp[, c("Winner", "Loser")])
+  # Pairs
+  tmp1 <- tmp[tmp$pocket_card1 == tmp$pocket_card2, ]
+  # Unique Suited Cards
+  tmp2 <- tmp[tmp$suited_pocket & tmp$pocket_card1 != tmp$pocket_card2, ]
+  tmp2 <- tmp2[tmp2$pocket_card1 <= tmp2$pocket_card2, ]
+  # Unique Non-suited Cards
+  tmp3 <- tmp[!tmp$suited_pocket & tmp$pocket_card1 != tmp$pocket_card2, ]
+  tmp3 <- tmp3[tmp3$pocket_card1 > tmp3$pocket_card2, ]
+  tmp.new <- rbind(tmp1, tmp2, tmp3)
+
+  # Control card values
+  tmp.new$pocket_card1 <- gsub(11, "Ja", tmp.new$pocket_card1)
+  tmp.new$pocket_card1 <- gsub(12, "Qu", tmp.new$pocket_card1)
+  tmp.new$pocket_card1 <- gsub(13, "Ki", tmp.new$pocket_card1)
+  tmp.new$pocket_card1 <- gsub(14, "Ac", tmp.new$pocket_card1)
+  tmp.new$pocket_card2 <- gsub(11, "Ja", tmp.new$pocket_card2)
+  tmp.new$pocket_card2 <- gsub(12, "Qu", tmp.new$pocket_card2)
+  tmp.new$pocket_card2 <- gsub(13, "Ki", tmp.new$pocket_card2)
+  tmp.new$pocket_card2 <- gsub(14, "Ac", tmp.new$pocket_card2)
+
+  # Number of players
+  tmp.new$Players <- max(table(x$hand_id[1:20])) # So we don't rely on player_id and don't have to table everything
+
+  # Return
+  tmp.new
+}))
+# Normalized versions of win percentages
+pocket_information <- pocket_information %>%
+  group_by(pocket_card1, pocket_card2) %>% mutate(
+    min_win_pct = round(min(pct_win)*100, 0),
+    max_win_pct = round(max(pct_win)*100, 0),
+    avg_win_pct = round(mean(pct_win)*100, 0)
+  ) %>%
+  group_by(Players) %>% mutate(
+    norm_win_pct = (min(pct_win) - pct_win)/(min(pct_win) - max(pct_win))
+  ) %>% ungroup()
+cards <- c("2", "3", "4", "5", "6", "7", "8", "9", "10", "Ja", "Qu", "Ki", "Ac")
+pocket_information$pocket_card1 <- factor(pocket_information$pocket_card1, levels = cards)
+pocket_information$pocket_card2 <- factor(pocket_information$pocket_card2, levels = cards)
+
+# Plot
+pocket_win_likeli <- lapply(unique(pocket_information$Players), function(x, df) {
+  title <- "Chance of Winning with Pocket Cards"
+  df <- df[df$Players == x, ]
+  df$Label <- round(df$pct_win*100, 0)
+  df$Label[df$pct_win <= quantile(df$pct_win, .75)] <- ""
+
+  # Plot
+  ggplot(data = df, aes(x = pocket_card1, y = pocket_card2)) +
+    geom_tile(aes(fill = suited_pocket)) +
+    geom_point(aes(color = pct_win, size = pct_win)) +
+    geom_text(aes(x = pocket_card1, y = pocket_card2, label = Label), fontface = "bold", size = 4, color = "black") +
+    scale_x_discrete(position = "top", limits = rev(levels(df$pocket_card1))) +
+    scale_size_continuous("Probability of\nWinning", guide = "legend", range = c(1, 10), labels = percent) +
+    scale_color_gradientn("Probability of\nWinning", guide = "legend", colours = plt_colorscale, labels = percent) +
+    scale_fill_manual("Are Cards Suited?", values = c("FALSE" = "#707070", "TRUE" = plt_background), breaks = "FALSE", labels = "No") +
+    # labs(title = title, subtitle = paste(x, "Players", sep = " "), x = "", y = "") +
+    labs(title = NULL, x = "", y = "") +
+    plt_theme
+}, df = pocket_information)
+pocket_win_likeli_8players <- pocket_win_likeli[[n_players_index]]
+
+pocket_win_likeli_all <- ggplot(data = pocket_information, aes(x = pocket_card1, y = pocket_card2)) +
+  geom_tile(aes(fill = suited_pocket)) +
+  geom_point(data = pocket_information %>% filter(Players < 10), aes(colour = pct_win, size = pct_win)) +
+  geom_point(data = pocket_information %>% filter(Players == 10), aes(colour = pct_win, size = pct_win), shape = 21, show.legend = FALSE, fill = plt_background) +
+  scale_x_discrete(position = "top", limits = rev(levels(pocket_information$pocket_card1))) +
+  scale_size_continuous("Probability of\nWinning", guide = "legend", range = c(1, 10), breaks = seq(0, 1, 0.2), labels = percent) +
+  scale_color_gradientn("Probability of\nWinning", guide = "legend", colours = plt_colorscale, breaks = seq(0, 1, 0.2), labels = percent) +
+  scale_fill_manual("Are Cards Suited?", values = c("FALSE" = "#707070", "TRUE" = plt_background), breaks = "FALSE", labels = "No") +
+  labs(title = NULL, x = "", y = "") +
+  plt_theme
+
+#########################################################################################################################
+# Plots not Used ########################################################################################################
+# Makers 1 - how much more likely am I to have a final hand type if I start with pocket pairs
+# pair_makers <- lapply(games_list, function(x, pocket_feature) {
+#   results <- as.data.frame(prop.table(table(x$final_hand_type, x[, pocket_feature, drop = TRUE]), margin = 2))
+#   colnames(results) <- c("Hand", pocket_feature, "Percentage")
+#   results
+# }, pocket_feature = "paired_pocket")
 #
-#   # Control card values
-#   tmp.new$pocket_card1 <- gsub(11, "Ja", tmp.new$pocket_card1)
-#   tmp.new$pocket_card1 <- gsub(12, "Qu", tmp.new$pocket_card1)
-#   tmp.new$pocket_card1 <- gsub(13, "Ki", tmp.new$pocket_card1)
-#   tmp.new$pocket_card1 <- gsub(14, "Ac", tmp.new$pocket_card1)
-#   tmp.new$pocket_card2 <- gsub(11, "Ja", tmp.new$pocket_card2)
-#   tmp.new$pocket_card2 <- gsub(12, "Qu", tmp.new$pocket_card2)
-#   tmp.new$pocket_card2 <- gsub(13, "Ki", tmp.new$pocket_card2)
-#   tmp.new$pocket_card2 <- gsub(14, "Ac", tmp.new$pocket_card2)
+# plot_data <- pair_makers[[7]]
+# ggplot(data = plot_data) +
+#   geom_bar(aes(x = Hand, y = Percentage, fill = paired_pocket), stat = "identity", position = "dodge") +
+#   plt_theme
 #
-#   # Number of players
-#   tmp.new$Players <- max(table(x$hand_id[1:20])) # So we don't rely on player_id and don't have to table everything
+# Makers 2 - how much more likely am I to have a final hand type if I start with suited pocket cards
+# suited_makers <- lapply(games_list, function(x, pocket_feature) {
+#   results <- as.data.frame(prop.table(table(x$final_hand_type, x[, pocket_feature, drop = TRUE]), margin = 2))
+#   colnames(results) <- c("Hand", pocket_feature, "Percentage")
+#   results
+# }, pocket_feature = "suited_pocket")
 #
-#   # Return
-#   tmp.new
-# }))
-# # Normalized versions of win percentages
-# pocket_information <- pocket_information %>%
-#   group_by(pocket_card1, pocket_card2) %>% mutate(
-#     min_win_pct = round(min(pct_win)*100, 0),
-#     max_win_pct = round(max(pct_win)*100, 0),
-#     avg_win_pct = round(mean(pct_win)*100, 0)
-#   ) %>%
-#   group_by(Players) %>% mutate(
-#     norm_win_pct = (min(pct_win) - pct_win)/(min(pct_win) - max(pct_win))
-#   ) %>% ungroup()
-# cards <- c("2", "3", "4", "5", "6", "7", "8", "9", "10", "Ja", "Qu", "Ki", "Ac")
-# pocket_information$pocket_card1 <- factor(pocket_information$pocket_card1, levels = cards)
-# pocket_information$pocket_card2 <- factor(pocket_information$pocket_card2, levels = cards)
+# plot_data <- suited_makers[[7]]
+# ggplot(data = plot_data) +
+#   geom_bar(aes(x = Hand, y = Percentage, fill = suited_pocket), stat = "identity", position = "dodge") +
+#   plt_theme
+
 # # Plots
-# pocket_win_likeli <- lapply(unique(pocket_information$Players), function(x, df) {
-#   title <- "Chance of Winning with Pocket Cards"
-#   df <- df[df$Players == x, ]
-#   df$Label <- round(df$pct_win*100, 0)
-#   df$Label[df$pct_win <= quantile(df$pct_win, .75)] <- ""
-#
-#   ggplot(data = df, aes(x = pocket_card1, y = pocket_card2)) +
-#     geom_tile(aes(fill = suited_pocket)) +
-#     geom_point(aes(color = pct_win, size = pct_win)) +
-#     geom_text(aes(x = pocket_card1, y = pocket_card2, label = Label), fontface = "bold", size = 7, color = "black") +
-#     scale_x_discrete(position = "top", limits = rev(levels(df$pocket_card1))) +
-#     scale_size_continuous("Probability of\nWinning", guide = "legend", range = c(1, 20), limits = c(0.2, 1), breaks = seq(0.2, 1, 0.2), labels = percent) +
-#     scale_color_gradientn("Probability of\nWinning", guide = "legend", colours = plt_colorscale, limits = c(0.2, 1), breaks = seq(0.2, 1, 0.2), labels = percent) +
-#     scale_fill_manual("Are Cards Suited?", values = c("FALSE" = "#707070", "TRUE" = plt_background), breaks = "FALSE", labels = "No") +
-#     labs(title = title, subtitle = paste(x, "Players", sep = " "), x = "", y = "") +
-#     plt_theme
-# }, df = pocket_information)
-#
 # title <- "Chance of Winning with Pocket Cards"
 # win_likeli_all <- ggplot(data = pocket_information, aes(x = pocket_card1, y = pocket_card2)) +
 #   geom_tile(aes(fill = suited_pocket)) +
@@ -350,8 +378,15 @@ win_distribution_10players <- hand_win_plots[[9]]
 
 #########################################################################################################################
 # Save Results
-save(hand_distributions_8players, win_distribution_2players, win_distribution_10players,
-     file = "Work/results/2020-01-TexasHoldem-Analysis-pt1.Rdata")
+save(
+  hand_distributions_8players,
+  win_distribution_2players, win_distribution_10players,
+  beaters_8players,
+  pocket_win_likeli_8players,
+  pocket_win_likeli_all,
+  # Output
+  file = "Work/results/2020-01-TexasHoldem-Analysis-pt1.Rdata"
+)
 
 print("END")
 #########################################################################################################################
